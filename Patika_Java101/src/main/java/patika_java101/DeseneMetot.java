a7b09a0fdaff8aec5c334ae78fafeda0d3e20da5
YagmurrrCan <74114217+YagmurrrCan@users.noreply.github.com>
2022-02-27 23:47:52 +0300

    Create DeseneMetot.java

---
+++ Patika_Java101/src/main/java/patika_java101/DeseneMetot.java
@@ -0,0 +1,49 @@
+/*
+ Java dilinde kullanıcıdan alınan n değerine göre aşağıdaki kurala uyan döngü kullanmadan bir "Recursive" metot yazın.
+Kural : Girilen sayı 0 veya negatif olduğu yere kadar girilen sayıdan 5 rakamını çıkarın. 
+Her çıkarma işlemi sırasında ekrana son değeri yazdırın. 
+Sayı negatif veya 0 olduktan sonra tekrar 5 ekleyin. 
+Yine her ekleme işleminde sayının son değerini ekrana yazdırın.
+
+Senaryolar:
+N Sayısı : 16
+Çıktısı : 16 11 6 1 -4 1 6 11 16 
+------------------------------------------------
+N Sayısı : 10
+Çıktısı : 10 5 0 5 10 
+------------------------------------------------
+N Sayısı : 25
+Çıktısı : 25 20 15 10 5 0 5 10 15 20 25 
+------------------------------------------------
+N Sayısı : 5
+Çıktısı : 5 0 5 
+ */
+package patika_java101;
+
+import java.util.Scanner;
+
+public class DeseneMetot {
+    
+    static void desen(int n){
+        System.out.print(n + " ");
+        if(n<=0){
+           return;
+        }
+        else {
+        desen(n - 5);
+        System.out.print(n + " ");
+        }
+        
+    }
+    
+        
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please give a number : ");
+        int number = scanner.nextInt();
+      
+        desen(number);
+    }
+    
+}
