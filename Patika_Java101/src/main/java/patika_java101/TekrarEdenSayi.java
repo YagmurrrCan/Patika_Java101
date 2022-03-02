4edfdf10671f31b3eb4adc20a18166029c2647c8
YagmurrrCan <74114217+YagmurrrCan@users.noreply.github.com>
2022-03-02 17:57:13 +0300

    Create TekrarEdenSayi.java

---
+++ Patika_Java101/src/main/java/patika_java101/TekrarEdenSayi.java
@@ -0,0 +1,40 @@
+/*
+ Bir sayı dizisindeki tekrar eden çift sayıları belirten bir program 
+ */
+package patika_java101;
+import java.util.Arrays;
+
+public class TekrarEdenSayi {
+    
+    static boolean isFind(int[] arr, int value){
+    for(int i : arr){
+        if(i == value){
+            return true;
+        }
+    }
+    return false;
+}
+    
+    public static void main(String[] args) {
+        int[] list = {3, 7, 3, 3, 2, 9, 10, 10, 1, 33, 9, 1};
+        int[] duplicate = new int[list.length];
+        int startIndex = 0;
+        
+        for(int i=0; i<list.length; i++){
+            for(int j=0; j<list.length; j++){
+                if((i!=j) && (list[i]==list[j])){
+                    if((!isFind(duplicate, list[i])) && (list[i]%2==0)){
+                        duplicate[startIndex++] = list[i];
+                    }
+                    break;
+                }
+            }
+        }
+        //System.out.println("Duplicate numbers : " + Arrays.toString(duplicate));
+        for(int value : duplicate){
+            if(value != 0){
+                System.out.println(value);
+            }
+        }
+    }
+}
