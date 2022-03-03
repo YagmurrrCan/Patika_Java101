e458c639535777b1079a0b3b91db02f4da10e50a
YagmurrrCan <74114217+YagmurrrCan@users.noreply.github.com>
2022-03-03 03:39:12 +0300

    Create PalindromikKelime.java

---
+++ Patika_Java101/src/main/java/patika_java101/PalindromikKelime.java
@@ -0,0 +1,42 @@
+/*
+Eğer bir kelimenin tersten okunuşu yine aynı kelimeyi veriyorsa o kelime "Palindromik Kelime'dir".
+Örnek : abba , asa , kavak, kayak
+ */
+package patika_java101;
+public class PalindromikKelime {
+    static boolean isPalindrome(String str) {
+        int i=0;
+        int j=str.length()-1;
+        
+        while (i<j) {
+            if(str.charAt(i) != str.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+    
+    // ========================= Başka Yöntem =========================
+    static boolean isPalindrome2(String str) {
+        String reverse = "";
+        for(int i=str.length()-1; i>=0; i--){
+        reverse += str.charAt(i);
+        }
+        
+        //return str.equals(reverse); yeter aslında çünkü boolean değer döndürür.
+        if(str.equals(reverse)){
+            return true;
+        } else {
+            return false;
+        }
+    }
+    //=================================================================
+    
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("kayak"));
+    }
+
+    
+}
